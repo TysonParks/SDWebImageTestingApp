24eb30c2076009f5143aa4382bdf080593771e60
Tyson Parks <typarks@gmail.com>
2020-11-02 12:47:22 -0800

    Initial Commit

---
+++ SDWebImageTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SDWebImageTesting
+//
+//  Created by Tyson Parks on 2020-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SDWebImageTesting/SDWebImageTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDWebImageTestingApp.swift
+//  SDWebImageTesting
+//
+//  Created by Tyson Parks on 2020-11-02.
+//
+
+import SwiftUI
+
+@main
+struct SDWebImageTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
